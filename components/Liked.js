db7008c1b2c9e44641ece7b3e26037d5f3b20ac3
Akmal Adnan <68601221+akmal578@users.noreply.github.com>
2021-01-31 23:59:06 +0800

    init

---
+++ components/Liked.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Liked() {
+  return (
+    <View>
+      <Text>Liked</Text>
+    </View>
+  );
+}
+
+export default Liked;
